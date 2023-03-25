7a0d1e52f8d6ddfcda94cc4c96c04e68a0873784
Mehmet Maden <yasamtr@gmail.com>
2023-03-26 00:23:19 +0300

    day32

---
+++ src/day32_dataTypeUsage/AnimalKingdom.java
@@ -0,0 +1,14 @@
+package day32_dataTypeUsage;
+
+public class AnimalKingdom {
+
+    String move = "they move";
+    String respiration= "they breathe";
+    String nutrition = "they are fed";
+    String multiply=  "they multiply";
+    String life= "they live and die";
+
+
+
+
+}
---
+++ src/day32_dataTypeUsage/Birds.java
@@ -0,0 +1,10 @@
+package day32_dataTypeUsage;
+
+public class Birds extends AnimalKingdom{
+
+    String wing = "they are winged";
+    String respiration= "they breathe with lungs";
+    String beak="they have beaks";
+    String multiply= "they multiply with eggs";
+
+}
